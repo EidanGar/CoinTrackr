245f25c0aa1bf49d2db8c7bfaa9b596e60e04f6c
Eidan Garcia <113457465+EidanGar@users.noreply.github.com>
2023-05-02 23:56:29 +0000

    Update 13 files

--- src/components/FooterSection.tsx
+++
@@ -1,7 +0,0 @@
-import { BsYoutube, BsTwitter, BsFacebook, BsDiscord } from "react-icons/bs";
-
-const FooterSection = () => {
-  return <div id="join" className="app__footer"></div>;
-};
-
-export default FooterSection;
--- src/components/TopSection.tsx
+++ src/components/TopSection.tsx
@@ -1,43 +1,53 @@
 import { Coin } from "../types";
 import { numberWithCommas } from "../helpers";
+import Loading from "./Loading";
 
-const TopSection = ({ coinData }: { coinData: Coin[] }) => {
+const TopSection = ({
+  coinData,
+  isLoading,
+}: {
+  coinData: Coin[];
+  isLoading: boolean;
+}) => {
   return (
     <section id="home" className="app__top-section">
       <h1 className="top-section__hero">
         Stay up-to-date on <span className="colored-text">Crypto prices</span>{" "}
         and <span className="colored-text">Market trends</span>
       </h1>
-      <div className="top-section__crypto">
-        {coinData?.slice(0, 4).map((coin, idx) => {
-          return (
-            <div className="top-section__crypto-coin" key={idx}>
-              <img
-                className="top-section__crypto-coin__image"
-                src={coin.image}
-                alt={coin.name}
-              />
-              <p>
-                {coin.name}{" "}
-                <span
-                  style={{
-                    color:
-                      +coin.price_change_percentage_24h < 0
-                        ? "#ff0000"
-                        : "#0ecb81"
-                  }}
-                >
-                  {numberWithCommas(
-                    +coin.price_change_percentage_24h.toFixed(2)
-                  )}
-                  %
-                </span>
-              </p>
-              <p>$ {numberWithCommas(+coin.current_price.toFixed(2))}</p>
-            </div>
-          );
-        })}
-      </div>
+      {isLoading && <Loading />}
+      {!isLoading && (
+        <div className="top-section__crypto">
+          {coinData?.slice(0, 4).map((coin, idx) => {
+            return (
+              <div className="top-section__crypto-coin" key={idx}>
+                <img
+                  className="top-section__crypto-coin__image"
+                  src={coin.image}
+                  alt={coin.name}
+                />
+                <p>
+                  {coin.name}{" "}
+                  <span
+                    style={{
+                      color:
+                        +coin.price_change_percentage_24h < 0
+                          ? "#ff0000"
+                          : "#0ecb81",
+                    }}
+                  >
+                    {numberWithCommas(
+                      +coin.price_change_percentage_24h.toFixed(2)
+                    )}
+                    %
+                  </span>
+                </p>
+                <p>$ {numberWithCommas(+coin.current_price.toFixed(2))}</p>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </section>
   );
 };
